4440e6ddd2497198d714e09d162ba34e1b883aa4
Luisitopr <zS22022234@estudiantes.uv.mx>
2024-09-08 22:14:34 -0600

    add interests branch #6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 import { HeaderComponent } from './header/header.component';
 import { EducationComponent } from './education/education.component';
+import { InterestsComponent } from './interests/interests.component';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,WorkExperienceComponent,HeaderComponent,EducationComponent],
+  imports: [RouterOutlet,WorkExperienceComponent,HeaderComponent,EducationComponent,InterestsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
